aa2e335168a676fa0ec33710b3c7879618a21a48
DavidLim93 <lim.david93@gmail.com>
2022-08-30 18:19:12 +1000

    Complete

--- js/challenge.js
+++ js/challenge.js
@@ -1,78 +1,82 @@
-// Dom Elements
 
-const timerH1 = document.querySelector("h1#counter");
-const buttonContainer = document.querySelector("button-container")
-const likesUl = document.querySelector("ul.likes")
-const commentForm=document.querySelector("#comment-form")
-const commentList=document.querySelector("#list")
 
-// Application state
-let currentNumber = 0;
-let counterRunning = true
-let likedNumbers = {}
+const counter = document.getElementById("counter");
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+const love = document.getElementById('heart')
+const comment = document.querySelector('.likes')
+const submit = document.getElementById('submit')
+const comments = document.getElementById('comment-input')
+const list = document.getElementById('list')
+const pause = document.getElementById('pause')
 
-commentForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+let counterRun = true
 
-    const p=document.createElement("p");
-    const input=document.querySelectorAll("#comment-input");
-    
-    p.textContent=input.values;
 
-    commentList.append(p);
 
-    event.target.reset();
-})
+function counterTimer () {
 
-buttonContainer.addEventListener("click", event => {
-    if (event.target.id === "plus") {
-        changeCounter(1)
-    } else if (event.target.id === "minus") {
-        changeCounter(-1)
-    } else if (event.target.id === "pause") {
-        togglePaused ()
-    } else if (event.target.id === "heart") {
-        updateLikes()
+    if (counterRun) {
+        counter.innerHTML++
     }
-})
-
-function updateLikes () {
-    if (likedNumbers[currentNumber]) {
-        const li = document.querySelectorAll(`[data-number="${currentNumber}"]`)
-        likedNumbers[likedNumbers] += 1
-        li.textContent = `The number ${currentNumber} has been liked ${likedNumbers[likedNumbers]} time`
-    } else {
-        likedNumbers[likedNumbers] = 1
-        const li = document.createElement("li")
-        li.dataset.number=currentNumber
-        li.textContent = `The number ${currentNumber} has been liked 1 time`
-         likesUl.append(li)
-    }
-  
 }
 
-function togglePaused () {
-    counterRunning = !counterRunning
-    document.querySelectorAll("button").forEach(button => {
-        if (button.id !== "pause") {
-            button.disabled=!counterRunning
-        } else {
-            if (counterRunning) {
-                button.textContent = "pause" 
-            } else {
-                button.textContent = "resume" 
-            }
-        }
+let timer = setInterval(counterTimer, 1000)
+
+document.addEventListener('DOMContentLoaded', () => {
+    timer
+})
+
+plus.addEventListener('click', () => {
+    counter.innerHTML++
+})
+
+minus.addEventListener('click', () => {
+    counter.innerHTML--
     })
-}
 
-function changeCounter(amount) {
-    currentNumber = currentNumber + amount;
-    timerH1.textContent = currentNumber
-}
+counterEle = {}
 
-setInterval (()=> {
-    if (counterRunning) {
-        changeCounter(1)
+love.addEventListener('click', () => {
+
+    if (counterEle[counter.innerHTML]){
+        listElement = document.querySelector(`[data-number= "${counter.innerHTML}"]`)
+        counterEle[counter.innerHTML] += 1
+        listElement.innerHTML = `${counter.innerHTML} was liked ${counterEle[counter.innerHTML]} times`
+        comment.append(listElement)
     }
-    } , 1000);
+    else {
+        listElement = document.createElement('li')
+        listElement.dataset.number = counter.innerHTML
+        counterEle[counter.innerHTML] = 1
+        listElement.innerHTML = `${counter.innerHTML} was liked ${counterEle[counter.innerHTML]} times`
+        comment.append(listElement)
+    }
+
+   
+})
+
+
+pause.addEventListener('click', () => {
+    if (counterRun) {
+        counterRun = false;
+        plus.disable = true;
+        minus.disable = true;
+        love.disable = true;
+        pause.innerHTML = 'Resume'
+    } else {
+        counterRun = true
+        plus.disable = false;
+        minus.disable = false;
+        love.disable = false;
+    }
+    const pause = document.getElementById('pause')
+    let counterRun = true
+})
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    pEle = document.createElement('p')
+    pEle.innerHTML = comments.value
+    list.append(pEle)
+})
